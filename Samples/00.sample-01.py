64fbd53630bb87444201f31ff088862e9da3980b
Anse Min <ansemin1025@gmail.com>
2022-10-14 11:52:23 +0900

    read

--- Samples/00.sample-01.py
+++ Samples/00.sample-01.py
@@ -1,5 +1,22 @@
+from ast import While
 import random
 
 random_number=random.randint(1,100)
 
-print(random_number)
+#print(random_number)
+
+game_count=1
+
+while True:
+
+
+        my_number=int(input("Choose number between 1_100"))
+
+        if my_number>random_number:
+            print('Down')
+        elif my_number<random_number:
+            print("Up")
+        else:
+            print(f'Congrat, you won in {game_count}tries')
+            break
+        game_count=game_count+1    
